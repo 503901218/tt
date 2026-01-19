b97ad7402a5bdd663325ffa26fa1aaf3506f68a2
lishan <503901218@qq.com>
2026-01-19 23:12:58 +0800

    commt

--- App.vue
+++ App.vue
@@ -6,8 +6,8 @@
 		onShow: function() {
 			console.log('App Show')
 		},
-		onHide: function() {
-			console.log('App Hide')
+		onHide: function() {//dddd
+			console.log('App Hide') 
 		}
 	}
 </script>
